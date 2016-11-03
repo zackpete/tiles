db182c58a95a241fe43ce5adc05e96216ec14e0d
Zack Peterson <zackpete@gmail.com>
2016-11-03 15:00:03 -0600

    Working well, now monitor configuration agnostic

--- extension.js
+++ extension.js
@@ -1,13 +1,12 @@
-const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
 const Meta = imports.gi.Meta;
-const Gio = imports.gi.Gio;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 const Shell = imports.gi.Shell;
+
+let spot_count = 1;
 
 function init() {
   let settings = Convenience.getSettings();
@@ -18,58 +17,62 @@ function init() {
   Main.wm.addKeybinding('tile-right', settings, Meta.KeyBindingFlags.NONE, modeType.NORMAL, right);
 }
 
-function up() { move(s => s.u) }
-function down() { move(s => s.d) }
-function left() { move(s => s.l) }
-function right() { move(s => s.r) }
+function up() { move('up') }
+function down() { move('down') }
+function left() { move('left') }
+function right() { move('right') }
 
-function resize(window, spot) {
-  if (spot) {
-    if (window.get_maximized()) {
-      window.unmaximize(3);
-      spot = spot.d || spot;
-    }
-
-    window.move_resize_frame(true, spot.x, spot.y, spot.w, spot.h);
-  } else {
-    if (!window.get_maximized()) window.maximize(3);
-  }
-}
-
-function move(movement) {
-  let spots = get_spots();
+function move(direction) {
+  spot_count = 1;
   let active_window = global.screen.get_display().get_focus_window();
   if (!active_window) return;
 
   let {x: x, y: y, width: w, height: h} = active_window.get_frame_rect();
-  let spot = get_closest_spot(spots, center(x, y, h, w));
+
+  let x_tweak = 0;
+  if (direction == 'left')  x_tweak = -10;
+  if (direction == 'right') x_tweak = 10;
+
+  let y_tweak = 0;
+  if (direction == 'up') y_tweak = -10;
+  if (direction == 'down') y_tweak = 10;
+
+  let spots = get_spots();
+  let spot = active_window.get_maximized() ?
+             get_closest_spot(spots, center(x + x_tweak, y + y_tweak, h - y_tweak, w - x_tweak)) :
+             get_closest_spot(spots, center(x, y, h, w));
   let norm = rect_norm(spot, {x: x, y: y, w: w, h: h});
 
-  if (norm > 10) {
-    resize(active_window, spot);
-    let {x: new_x, y: new_y, width: new_w, height: new_h} = active_window.get_frame_rect();
-    if (x != new_x || y != new_y || h != new_h || w != new_w) {
-      return;
-    }
+  let next;
+  if (norm > 20) {
+    next = spot;
+  } else if (direction == 'up' && !spot.u) {
+    active_window.maximize(3);
+    return;
+  } else if (direction == 'down' && !spot.d) {
+    active_window.minimize();
+    return;
+  } else {
+    next = spot_direction(spot, direction);
   }
 
-  resize(active_window, movement(spot));
-}
-
-function rect_norm(rect_one, rect_two) {
-  return Math.sqrt(
-      Math.pow(rect_one.x - rect_two.x, 2) +
-      Math.pow(rect_one.y - rect_two.y, 2) +
-      Math.pow(rect_one.h - rect_two.h, 2) +
-      Math.pow(rect_one.w - rect_two.w, 2)
-  );
+  if (!next) return;
+  if (active_window.get_maximized) active_window.unmaximize(3);
+  active_window.move_resize_frame(true, next.x, next.y, next.w, next.h);
+  active_window.raise();
+  let {x: new_x, y: new_y, width: new_w, height: new_h} = active_window.get_frame_rect();
+  if (x != new_x || y != new_y || h != new_h || w != new_w) return;
+  next = spot_direction(spot, direction);
+  if (!next) return;
+  active_window.move_resize_frame(true, next.x, next.y, next.w, next.h);
+  active_window.raise();
 }
 
 function get_closest_spot(spots, point) {
   let closest_distance = Number.POSITIVE_INFINITY;
-  let closest_spot = undefined;
+  let closest_spot;
   for (let i = 0; i < spots.length; i++) {
-    let d = distance(point, spots[i].c);
+    let d = distance_points(point, spots[i].c);
     if (d < closest_distance) {
       closest_distance = d;
       closest_spot = spots[i];
@@ -79,96 +82,155 @@ function get_closest_spot(spots, point) {
   return closest_spot;
 }
 
+
 function get_spots() {
   let spots = [];
   let workspace = global.screen.get_active_workspace();
-
   let n = global.screen.get_n_monitors();
-  let dimensions = [];
   for (let i = 0; i < n; i++) {
     let r = workspace.get_work_area_for_monitor(i);
-    // global.log('loading: x:' + r.x + ' y:' + r.y + ' h:' + r.height + ' w:' + r.width);
-    dimensions.push(r);
+    spots = spots.concat(split(r));
   }
 
-  // Sort so they are left to right
-  dimensions.sort(function (a, b) {
-    return a.x - b.x;
-  });
-
-  let last_spot_top = undefined;
-  let last_spot_bottom = undefined;
-  for (let i = 0; i < n; i++) {
-    let x = dimensions[i].x;
-    let y = dimensions[i].y;
-    let h = dimensions[i].height;
-    let w = dimensions[i].width;
-
-    let spot1 = {};
-    let spot2 = {};
-    if (w > h) { // landscape
-      spot1.x = x;
-      spot1.y = spot2.y = y;
-      spot1.h = spot2.h = h;
-      spot1.w = parseInt(w / 2);
-      spot2.x = x + spot1.w;
-      spot2.w = w - spot1.w;
-
-      spot1.r = spot2;
-      spot1.l = last_spot_top;
-      spot2.l = spot1;
-
-      if (last_spot_top) {
-        last_spot_top.r = spot1;
-      }
-
-      if (last_spot_bottom) {
-        last_spot_bottom.r = spot1;
-      }
-
-      last_spot_top = spot2;
-      last_spot_bottom = spot2;
-    } else { // portrait
-      spot1.x = spot2.x = x;
-      spot1.y = y;
-      spot1.h = parseInt(h / 2);
-      spot1.w = spot2.w = w;
-      spot2.y = y + spot1.h;
-      spot2.h = h - spot1.h;
-
-      spot1.d = spot2;
-      spot1.l = last_spot_top;
-      spot2.u = spot1;
-      spot2.l = last_spot_bottom;
-
-      if (last_spot_top) {
-        last_spot_top.r = spot1;
-      }
-
-      if (last_spot_bottom) {
-        last_spot_bottom.r = spot2;
-      }
-
-      last_spot_top = spot1;
-      last_spot_bottom = spot2;
-    }
-
-    spot1.c = center(spot1.x, spot1.y, spot1.h, spot1.w);
-    spot2.c = center(spot2.x, spot2.y, spot2.h, spot2.w);
-
-    spots.push(spot1);
-    spots.push(spot2);
+  for (let i = 0; i < spots.length; i++) {
+    set_directional_spots(spots[i], spots);
   }
+
+  for (let i = 0; i < spots.length; i++) {
+    dbg(spots[i]);
+  }
+  return spots;
+}
+
+function split(rect) {
+  let {x: x, y: y, height: h, width: w} = rect;
+
+  let spot1 = {label: spot_count++};
+  let spot2 = {label: spot_count++};
+  let spots = [spot1, spot2];
+  if (w > h) { // landscape
+    spot1.x = x;
+    spot1.y = spot2.y = y;
+    spot1.h = spot2.h = h;
+    spot1.w = parseInt(w / 2);
+    spot2.x = x + spot1.w;
+    spot2.w = w - spot1.w;
+  } else { // portrait
+    spot1.x = spot2.x = x;
+    spot1.y = y;
+    spot1.h = parseInt(h / 2);
+    spot1.w = spot2.w = w;
+    spot2.y = y + spot1.h;
+    spot2.h = h - spot1.h;
+  }
+
+  spot1.c = center(spot1.x, spot1.y, spot1.h, spot1.w);
+  spot2.c = center(spot2.x, spot2.y, spot2.h, spot2.w);
 
   return spots;
+}
+
+function dbg(spot) {
+  global.log('spot #' + spot.label);
+  global.log('x:' + spot.x + ' y:' + spot.y + ' h:' + spot.h + ' w:' + spot.w + ' c:' +
+             JSON.stringify(spot.c));
+  let dir = "";
+  if (spot.u) dir += ' up:' + spot.u.label;
+  if (spot.d) dir += ' down:' + spot.d.label;
+  if (spot.l) dir += ' left:' + spot.l.label;
+  if (spot.r) dir += ' right:' + spot.r.label;
+  global.log(dir);
+}
+
+
+function set_directional_spots(spot, all) {
+  let closest = {
+    up: {distance: Number.POSITIVE_INFINITY},
+    down: {distance: Number.POSITIVE_INFINITY},
+    left: {distance: Number.POSITIVE_INFINITY},
+    right: {distance: Number.POSITIVE_INFINITY}
+  };
+
+  for (let i = 0; i < all.length; i++) {
+    let other = all[i];
+    if (spot == other) continue;
+
+    let direction = Math.atan2(other.c.y - spot.c.y, other.c.x - spot.c.x) + Math.PI;
+    let distance = distance_points(spot.c, other.c);
+    let debugs = ""
+    debugs +=
+        'direction from ' + spot.label + ' to ' + other.label + ': ' + (direction * 180 / Math.PI);
+    debugs += ' distance: ' + distance;
+
+    if (direction < Math.PI / 4 || direction >= 7 / 4 * Math.PI) {
+      debugs += ' LEFT';
+      if (distance < closest.left.distance) {
+        debugs += ' *SELECTED*';
+        closest.left.spot = other;
+        closest.left.distance = distance;
+      }
+    } else if (direction < 3 / 4 * Math.PI) {
+      debugs += ' UP';
+      if (distance < closest.up.distance) {
+        debugs += ' *SELECTED*';
+        closest.up.spot = other;
+        closest.up.distance = distance;
+      }
+    } else if (direction < 5 / 4 * Math.PI) {
+      debugs += ' RIGHT';
+      if (distance < closest.right.distance) {
+        debugs += ' *SELECTED*';
+        closest.right.spot = other;
+        closest.right.distance = distance;
+      }
+    } else {
+      debugs += ' DOWN';
+      if (distance < closest.down.distance) {
+        debugs += ' *SELECTED*';
+        closest.down.spot = other;
+        closest.down.distance = distance;
+      }
+    }
+
+    global.log(debugs);
+  }
+
+  spot.u = closest.up.spot;
+  spot.d = closest.down.spot;
+  spot.l = closest.left.spot;
+  spot.r = closest.right.spot;
+}
+
+function spot_direction(spot, direction) {
+  switch (direction) {
+    case 'up':
+      return spot.u;
+    case 'down':
+      return spot.d;
+    case 'left':
+      return spot.l;
+    case 'right':
+      return spot.r;
+  }
+
+  return undefined;
 }
 
 function center(x, y, h, w) {
   return {x: x + w / 2, y: y + h / 2};
 }
 
-function distance(p1, p2) {
+function distance_points(p1, p2) {
   return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+}
+
+function rect_norm(rect_one, rect_two) {
+  return Math.sqrt(
+      Math.pow(rect_one.x - rect_two.x, 2) +
+      Math.pow(rect_one.y - rect_two.y, 2) +
+      Math.pow(rect_one.h - rect_two.h, 2) +
+      Math.pow(rect_one.w - rect_two.w, 2)
+  );
 }
 
 function enable() { }
